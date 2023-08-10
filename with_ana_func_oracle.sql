44cf8c90100e7a9553b926cc2aec8c85ef6e911f
ideeep <52308265+ideeep@users.noreply.github.com>
2023-08-10 20:58:25 +0530

    Add files via upload

---
+++ with_ana_func_oracle.sql
@@ -0,0 +1,51 @@
+WITH cte_anal_func AS (
+    SELECT
+        e.*,
+        FIRST_VALUE(salary)
+        OVER(PARTITION BY e.department_id
+            ORDER BY
+                e.salary DESC
+            RANGE BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING
+        )              AS highest_val,
+        LAST_VALUE(salary)
+        OVER(PARTITION BY e.department_id
+            ORDER BY
+                e.salary DESC
+            RANGE BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING
+        )              AS lowest_val,
+        NTH_VALUE(salary, 1)
+        OVER(PARTITION BY e.department_id
+            ORDER BY
+                e.salary DESC
+            RANGE BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING
+        )              AS nth_val,
+        NTILE(3)
+        OVER(
+            ORDER BY salary DESC
+        )              AS ntile_val,
+        round(CUME_DIST()
+              OVER(
+            ORDER BY salary DESC
+              ), 2)          AS cum_dist,
+        round(PERCENT_RANK()
+              OVER(
+            ORDER BY salary ASC
+              ), 2) * 100      AS percentage_rank
+    FROM
+        employees e
+)
+SELECT
+    cte_anal_func.*,
+        CASE
+        WHEN cte_anal_func.ntile_val = 1    THEN
+            'Lesser'
+        WHEN cte_anal_func.ntile_val = 2    THEN
+            'good'
+        WHEN cte_anal_func.ntile_val = 3    THEN
+            'best'
+    END categ
+FROM
+    cte_anal_func
+WHERE
+        1 = 1
+    AND salary = 2800
